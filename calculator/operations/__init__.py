e1a05fdcd96868cf0236299b2676cb1c79a512c3
nick <nc363@njit.edu>
2022-03-28 21:16:18 -0400

    Project 2 !

--- calculator/operations/__init__.py
+++ calculator/operations/__init__.py
@@ -1,17 +1,30 @@
-class Addition:
+""" These are the Operation Classes"""
 
+
+class Addition:
+    """ This is the addition class"""
+
+    # this defines a static method that you can use without instantiating the calculator class
+    # If you can go to the store and buy it, it is an an object.  If you can't buy it then its a static method
     @staticmethod
     def add(value_1, value_2):
+        """ This is the add method"""
         return value_1 + value_2
 
 
-class Subtract:
+class Subtraction:
+    """ This is the subtraction class"""
+
     @staticmethod
     def subtract(value_1, value_2):
+        """ This is the add method"""
         return value_1 - value_2
 
 
-class Multiply:
+class Multiplication:
+    """ This is the subtraction class"""
+
     @staticmethod
     def multiply(value_1, value_2):
+        """ This is the add method"""
         return value_1 * value_2
--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -11,6 +11,11 @@ def test_request_main_menu_links(client):
     assert b'<a href="/page2" class="nav-link">Docker</a>' in response.data
     assert b'<a href="/page3" class="nav-link">Python/Flask</a>' in response.data
     assert b'<a href="/page4" class="nav-link">Continuous Integration</a>' in response.data
+    assert b'<a href="/page5" class="nav-link">OOP</a>' in response.data
+    assert b'<a href="/page6" class="nav-link">AAA</a>' in response.data
+    assert b'<a href="/page7" class="nav-link">Calculator Principles</a>' in response.data
+    assert b'<a href="/page8" class="nav-link">Calculator Design</a>' in response.data
+    assert b'<a href="/page9" class="nav-link">Pylint</a>' in response.data
 
 
 def test_request_index(client):
@@ -49,8 +54,37 @@ def test_request_page4(client):
     assert response.status_code == 200
 
 
-def test_request_page_not_found(client):
+def test_request_page5(client):
     """This makes the index page"""
     response = client.get("/page5")
-    assert response.status_code == 404
+    assert response.status_code == 200
 
+
+def test_request_page6(client):
+    """This makes the index page"""
+    response = client.get("/page6")
+    assert response.status_code == 200
+
+
+def test_request_page7(client):
+    """This makes the index page"""
+    response = client.get("/page7")
+    assert response.status_code == 200
+
+
+def test_request_page8(client):
+    """This makes the index page"""
+    response = client.get("/page8")
+    assert response.status_code == 200
+
+
+def test_request_page9(client):
+    """This makes the index page"""
+    response = client.get("/page9")
+    assert response.status_code == 200
+
+
+def test_request_page_not_found(client):
+    """This makes the index page"""
+    response = client.get("/page10")
+    assert response.status_code == 404
